0ea7beba0ba6dece287e47070907518b77a7a2a7
Nihsadd <nihsadd@gmail.com>
2024-05-25 12:36:14 -0600

    fix filepath structure

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,29 +3,26 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: '/src/js/index.js',
+      install: '/src/js/install.js',
+      database: '/src/js/database.js',
+      editor: '/src/js/editor.js',
+      header: '/src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      // Generate HTML file
       new HtmlWebpackPlugin({
-        template: './src/index.html',
+        template: './index.html',
         filename: 'index.html',
         chunks: ['main'],
       }),
-
-      // generate manifest file for PWA
       new WebpackPwaManifest({
         name: 'Just Another Text Editor',
         short_name: 'J.A.T.E',
@@ -34,28 +31,23 @@ module.exports = () => {
         theme_color: '#ffffff',
         icons: [
           {
-            src: path.resolve('src/img/icon.png'),
+            src: path.resolve(__dirname, 'src/images/logo.png'), // Corrected path
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('icons'),
           },
         ],
       }),
-
-      // inject service worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'service-worker.js',
       }),
     ],
-
     module: {
       rules: [
-        // CSS loader
         {
           test: /\.css$/,
           use: ['style-loader', 'css-loader'],
         },
-        // Babel loader
         {
           test: /\.js$/,
           exclude: /node_modules/,
