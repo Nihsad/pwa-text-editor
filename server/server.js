8c3bf582f8f0dd02d41204eb1132b869051d8c68
Nihsadd <nihsadd@gmail.com>
2024-06-09 18:36:05 -0600

    csp edits

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const helmet = require('helmet');
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Set Content Security Policy headers
+// Use helmet middleware to set Content Security Policy headers
 app.use(
   helmet.contentSecurityPolicy({
+    useDefaults: true,
     directives: {
-      defaultSrc: ["'self'"],
-      // Add directives as needed based on your requirements
+      scriptSrc: ["'self'", "'unsafe-inline'", "https://cdnjs.cloudflare.com"],
+      styleSrc: ["'self'", "'unsafe-inline'", "https://cdnjs.cloudflare.com"],
     },
   })
 );
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
