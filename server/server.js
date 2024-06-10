85927b2f64b3948a889bce0c2d0b5eada1197abc
Nihsadd <nihsadd@gmail.com>
2024-06-09 18:22:04 -0600

    more trying to fix service worker

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+// Set Content Security Policy headers
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      // Add directives as needed based on your requirements
+    },
+  })
+);
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
