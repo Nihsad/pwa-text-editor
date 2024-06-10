da94f579d8f49a0b9764c9b22f1821f67ff267ba
Nihsadd <nihsadd@gmail.com>
2024-06-09 18:45:26 -0600

    remove csp components

--- server/server.js
+++ server/server.js
@@ -1,19 +1,7 @@
 const express = require('express');
-const helmet = require('helmet');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Use helmet middleware to set Content Security Policy headers
-app.use(
-  helmet.contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-      scriptSrc: ["'self'", "'unsafe-inline'", "https://cdnjs.cloudflare.com"],
-      styleSrc: ["'self'", "'unsafe-inline'", "https://cdnjs.cloudflare.com"],
-    },
-  })
-);
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
